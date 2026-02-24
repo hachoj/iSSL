1de4745d594b91047ff845bd348666d54cb2b83e
hachoj <hjchojnowski@gmail.com>
2026-02-23 21:31:56 -0500

    made attentive_probe module

---
+++ tests/utils/attentive_probe.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange, repeat
+from jaxtyping import Float
+from torch import Tensor
+
+
+class attentive_probe(nn.Module):
+    def __init__(self, dim: int, num_classes: int):
+        super().__init__()
+
+        # this is fixes as per https://arxiv.org/abs/2502.08769
+        self.head_dim = 64
+        assert dim % self.head_dim == 0, "dim needs to be divisible by 64"
+        self.num_heads = dim // self.head_dim
+
+        self.query_token = nn.Parameter(torch.randn(dim))
+
+        self.kv_proj = nn.Linear(dim, 2 * dim)
+        self.linear_out = nn.Linear(dim, num_classes)
+
+        self.apply(self._init_weights)
+
+    def forward(self, x: Float[Tensor, "B N C"]):
+        B = x.shape[0]
+        x = rearrange(self.kv_proj(x), "B N (two C) -> two B N C", two=2)
+        k, v = x[0], x[1]
+
+        q = repeat(self.query_token, "C -> B C", B=B)
+
+        x = F.scaled_dot_product_attention(query=q, key=k, value=v)
+
+        return self.linear_out(x)
+
+    def _init_weights(self, module: nn.Module):
+        if isinstance(module, nn.Linear):
+            nn.init.trunc_normal_(module.weight, std=0.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
